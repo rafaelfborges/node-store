8faede18129e42a150c98b15943e73b3199e0dba
Rafael Borges <rafaelfborges@gmail.com>
2020-04-11 08:34:57 -0300

    feat: Add auth service using jwt

---
+++ src/services/AuthService.js
@@ -0,0 +1,30 @@
+'use strict';
+const jwt = require('jsonwebtoken');
+
+exports.generateToken = async (data) => {
+  return jwt.sign(data, global.SALT_KEY, { expiresIn: '1d' });
+}
+
+exports.decodeToken = async (token) => {
+  return await jwt.verify(token, global.SALT_KEY);
+}
+
+exports.authorize = (req, res, next) => {
+  const token = req.body.toke || req.query.token || req.headers['x-access-token'];
+
+  if (!token) {
+    res.status(401).json({
+      message: 'Acesso Restrito'
+    });
+  } else {
+    jwt.verify(token, global.SALT_KEY, (error, decoded) => {
+      if (error) {
+        res.status(401).json({
+          message: 'Token Inválido'
+        });
+      } else {
+        next();
+      }
+    });
+  }
+}
