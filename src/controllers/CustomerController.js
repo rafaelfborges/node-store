094de9d0c116dfb6453f0f2c4515427e9444f0b1
Rafael Borges <rafaelfborges@gmail.com>
2020-04-11 22:52:42 -0300

    feat: Autorization control based on roles

--- src/controllers/CustomerController.js
+++ src/controllers/CustomerController.js
@@ -10,7 +10,8 @@ exports.post = async (req, res, next) => {
     await repository.create({
       name: req.body.name,
       email: req.body.email,
-      password: md5(req.body.password + global.SALT_KEY)
+      password: md5(req.body.password + global.SALT_KEY),
+      roles: ["user"]
     });
 
     emailService.send(
@@ -46,14 +47,16 @@ exports.authenticate = async (req, res, next) => {
     const token = await authService.generateToken({
       id: customer._id,
       email: customer.email,
-      name: customer.name
+      name: customer.name,
+      roles: customer.roles
     })
 
     res.status(201).send({
       token: token,
       data: {
         email: customer.email,
-        name: customer.name
+        name: customer.name,
+        roles: customer.roles
       }
     });
   } catch (e) {
@@ -80,7 +83,8 @@ exports.refreshToken = async (req, res, next) => {
     const tokenData = await authService.generateToken({
       id: customer._id,
       email: customer.email,
-      name: customer.name
+      name: customer.name,
+      roles: customer.roles
     })
 
     res.status(201).send({
--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -28,3 +28,29 @@ exports.authorize = (req, res, next) => {
     });
   }
 }
+
+exports.isAdmin = (req, res, next) => {
+  const token = req.body.toke || req.query.token || req.headers['x-access-token'];
+
+  if (!token) {
+    res.status(401).json({
+      message: 'Acesso Restrito'
+    });
+  } else {
+    jwt.verify(token, global.SALT_KEY, (error, decoded) => {
+      if (error) {
+        res.status(401).json({
+          message: 'Token Inválido'
+        });
+      } else {
+        if (decoded.roles.includes('admin')) {
+          next();
+        } else {
+          res.status(403).json({
+            message: 'Esta funcionalidade é restrita para administradores'
+          });
+        }
+      }
+    });
+  }
+}
