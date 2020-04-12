de8da6a007a8ef3e81f13817119e4699f6d8a130
Rafael Borges <rafaelfborges@gmail.com>
2020-04-11 22:31:24 -0300

    feat: Customer refresh token

--- src/controllers/CustomerController.js
+++ src/controllers/CustomerController.js
@@ -43,8 +43,6 @@ exports.authenticate = async (req, res, next) => {
       return;
     }
 
-    console.log(customer)
-
     const token = await authService.generateToken({
       id: customer._id,
       email: customer.email,
@@ -53,6 +51,40 @@ exports.authenticate = async (req, res, next) => {
 
     res.status(201).send({
       token: token,
+      data: {
+        email: customer.email,
+        name: customer.name
+      }
+    });
+  } catch (e) {
+    res.status(500).send({
+      message: 'Falha ao processar sua requisição'
+    });
+  }
+};
+
+exports.refreshToken = async (req, res, next) => {
+  try {
+    const token = req.body.token || req.query.token || req.headers['x-access-token'];
+    const data = await authService.decodeToken(token);
+
+    const customer = await repository.getById(data.id);
+
+    if (!customer) {
+      res.status(404).send({
+        message: 'Cliente não encontrado'
+      });
+      return;
+    }
+
+    const tokenData = await authService.generateToken({
+      id: customer._id,
+      email: customer.email,
+      name: customer.name
+    })
+
+    res.status(201).send({
+      token: tokenData,
       data: {
         email: customer.email,
         name: customer.name
