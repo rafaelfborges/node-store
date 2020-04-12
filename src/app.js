132575eba8a292702b32c35961f97d7af07a2c31
Rafael Borges <rafaelfborges@gmail.com>
2020-04-11 23:00:56 -0300

    fix: Configure CORS and limit BodyParser at 5mb

--- src/app.js
+++ src/app.js
@@ -24,8 +24,20 @@ const productRoute = require('./routes/ProductRoute');
 const customerRoute = require('./routes/CustomerRoute');
 const orderRoute = require('./routes/OrderRoute');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({
+  limit: '5mb'
+}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Habilita o CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+})
 
 app.use('/', indexRoute);
 app.use('/products', productRoute);
